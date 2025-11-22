99f62f8e9c9f7f67994de60e106b7b3979417a7a
Liam Sullivan Hickey <hickey.liams@gmail.com>
2025-11-22 18:18:40 -0500

    price level add logic

--- include/Orderbook.h
+++ include/Orderbook.h
@@ -24,7 +24,7 @@ public:
 
 private:
 
-    MemoryPool<Order*> orderPool;
+    MemoryPool<Order> orderPool;
     MemoryPool<PriceLevelOrders*> priceLevelPool;
     PriceLevelOrders* bids_ = nullptr;
     PriceLevelOrders* asks_ = nullptr;
@@ -39,53 +39,88 @@ private:
 
     void addOrder(Order* order) noexcept {
         //this will be called if our order is only partially filled, or not filled at all
-
         const auto levelOrders = getPriceLevel(order->price_);
         if (!levelOrders){
             order->nextOrder_ = order->prevOrder_ = order;
             auto newPriceLevel = priceLevelPool.Allocate(order->side_, order->price_, order, nullptr, nullptr);
-            addToPriceLevel(*newPriceLevel);
+            addPriceLevel(*newPriceLevel);
         } else {
-
+            auto BestSideLevel = (order->side_ == Side::BUY) ? bids_ : asks_;
+            while (!priceLevelCompare(order->side_, BestSideLevel->price_, order->price_)){
+                BestSideLevel = BestSideLevel->nextPrice_;
+            }
+            //once this is true, we have found the correct side Level now we need to add order to the back
+            //
+            addOrderToTail(order, BestSideLevel);
         }
     }
 
-    void addToPriceLevel(PriceLevelOrders* newLevel) noexcept {
+    void addOrderToTail(Order* newOrder, PriceLevelOrders* orderPriceLevel){
+        auto tail = orderPriceLevel->headOrder->prevOrder_;
+        auto head = orderPriceLevel->headOrder;
+        tail->nextOrder_ = newOrder;
+        newOrder->prevOrder_ = tail;
 
+        newOrder->nextOrder_ = head;
+        head->prevOrder_ = newOrder;
+    }
+
+    void addPriceLevel(PriceLevelOrders* newLevel) noexcept {
+        //add to price level Hash map
+        // find the spot where this price level can Rest
+        auto& bestSideLevel = (newLevel->side_ == Side::BUY) ? bids_ : asks_;
+
+        if (!bestSideLevel) [[unlikely]]{
+            bestSideLevel = newLevel;
+            newLevel->prevPrice_ = newLevel->nextPrice_ = newLevel;
+            return;
+        }
+
+        auto currentPriceLevel = bestSideLevel;
+        if (priceLevelCompare(newLevel->side_, currentPriceLevel->price_, newLevel->price_)){
+            insertLevelBefore(bestSideLevel,newLevel);
+            bestSideLevel = newLevel;
+            return;
+        }
+
+        currentPriceLevel = currentPriceLevel->nextPrice_;
+        while (currentPriceLevel != bestSideLevel && !priceLevelCompare(newLevel->side_, currentPriceLevel->price_, newLevel->price_)){
+            currentPriceLevel = currentPriceLevel->nextPrice_;
+        }
+
+        insertLevelBefore(currentPriceLevel, newLevel);
+    }
+
+    void insertLevelBefore(PriceLevelOrders* currentLevel, PriceLevelOrders* newLevel){
+        auto prev = currentLevel->prevPrice_;
+
+        newLevel->nextPrice_ = currentLevel;
+        newLevel->prevPrice_ = prev;
+
+        currentLevel->prevPrice_ = newLevel;
+        prev->nextPrice_ = newLevel;
+    }
+
+    bool priceLevelCompare(Side side, Price currentPrice, Price newLevelPrice){
+        if (side == Side::BUY){
+            return currentPrice < newLevelPrice;
+        } else {
+            return currentPrice > newLevelPrice;
+        }
     }
 
     size_t priceToIndex(Price price) const {
         return price % MAXLEVELS;
     }
 
+    PriceLevelOrders* getSide(Side side) const {
+        return (side == Side::BUY) ? bids_ : asks_;
+    }
+
     PriceLevelOrders* getPriceLevel(Price price) const noexcept {
         return priceLevelsMap.at(priceToIndex(price));
     }
 
-    //find price level
     void removePriceLevel(Price price) noexcept {}
-
-    bool priceCrosses(Side side, Price price) const noexcept {
-        if (side == Side::BUY){
-            return price > asks_->price_;
-        } else {
-            //if sell, bid should be higher than sell
-            return price < bids_->price_;
-        }
-    }
-
-    //match will be called from within add IF we determine priceCrosses
-    auto match(Side side, Price price, Quantity quantity) noexcept {
-        Quantity remainingQuantity; //we can return this to our add function
-        //in this setup, we will actually just match based on an agressor
-        if (side == Side::BUY){
-
-        } else {
-
-        }
-
-        return remainingQuantity;
-    }
-
 
 };
--- src/Orderbook.cpp
+++ src/Orderbook.cpp
@@ -3,8 +3,14 @@
 
 void Orderbook::add(OrderId orderId, Side side, Price price, Quantity quantity){
     //in must allocate an orderFirst
-    auto newOrder = orderPool.Allocate(orderId,side,price,quantity);
-
+    Order* newOrder = orderPool.Allocate(orderId,side,price,quantity);
+    //once order is Added, now we have to place it into the orderMap, given Orders are sequential we can simply place it at the next index
+    if (nextOrderId != newOrder->orderId_){
+        //error here, maybe try to ASSERT so we don't deal with this at run time
+    }
+    orderMap[newOrder->orderId_] = newOrder;
+    ++nextOrderId;
+    addOrder(newOrder);
 }
 
 void Orderbook::deleteOrder(OrderId orderId){}
